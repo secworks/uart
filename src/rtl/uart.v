83564f7935ad66253fdbbab9a5c1005b2e26756a
Joachim Strömbergson <joachim@secworks.se>
2014-05-09 13:06:19 +0200

    Fixed (c) comma.

--- src/rtl/uart.v
+++ src/rtl/uart.v
@@ -18,7 +18,7 @@
 //
 //
 // Author: Joachim Strombergson
-// Copyright (c) 2014  Secworks Sweden AB
+// Copyright (c) 2014, Secworks Sweden AB
 // 
 // Redistribution and use in source and binary forms, with or 
 // without modification, are permitted provided that the following 
